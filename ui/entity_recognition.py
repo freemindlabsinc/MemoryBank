5844e7a8928c3dfe379827fc31c74c23f9aa6907
Alessandro Federici <afederici75@users.noreply.github.com>
2024-03-06 20:20:35 -0500

    Added summarization and entity_recognition tabs

---
+++ ui/entity_recognition.py
@@ -0,0 +1,50 @@
+import gradio as gr
+from transformers import pipeline
+
+text = ('''The tower is 324 metres (1,063 ft) tall, about the same height
+        as an 81-storey building, and the tallest structure in Paris. 
+        Its base is square, measuring 125 metres (410 ft) on each side. 
+        During its construction, the Eiffel Tower surpassed the Washington 
+        Monument to become the tallest man-made structure in the world,
+        a title it held for 41 years until the Chrysler Building
+        in New York City was finished in 1930. It was the first structure 
+        to reach a height of 300 metres. Due to the addition of a broadcasting 
+        aerial at the top of the tower in 1957, it is now taller than the 
+        Chrysler Building by 5.2 metres (17 ft). Excluding transmitters, the 
+        Eiffel Tower is the second tallest free-standing structure in France 
+        after the Millau Viaduct.''')
+
+get_completion = pipeline("ner", model="dslim/bert-base-NER")
+
+def merge_tokens(tokens):
+    merged_tokens = []
+    for token in tokens:
+        if merged_tokens and token['entity'].startswith('I-') and merged_tokens[-1]['entity'].endswith(token['entity'][2:]):
+            # If current token continues the entity of the last one, merge them
+            last_token = merged_tokens[-1]
+            last_token['word'] += token['word'].replace('##', '')
+            last_token['end'] = token['end']
+            last_token['score'] = (last_token['score'] + token['score']) / 2
+        else:
+            # Otherwise, add the token to the list
+            merged_tokens.append(token)
+
+    return merged_tokens
+
+def ner(input):
+    output = get_completion(input)#, parameters=None, ENDPOINT_URL=API_URL)
+    merged_tokens = merge_tokens(output)
+    return {"text": input, "entities": merged_tokens}
+
+
+def create_entity_recognition_tab():
+    gr.close_all()
+    tab = gr.Interface(fn=ner,
+                    inputs=[gr.Textbox(label="Text to find entities", lines=2, value=text)],
+                    outputs=[gr.HighlightedText(label="Text with entities")],
+                    title="NER with dslim/bert-base-NER",
+                    description="Find entities using the `dslim/bert-base-NER` model under the hood!",
+                    allow_flagging="never",
+                    examples=["My name is Andrew, I'm building DeeplearningAI and I live in California", "My name is Poli, I live in Vienna and work at HuggingFace"])
+
+    return tab
--- ui/summarization.py
+++ ui/summarization.py
@@ -3,18 +3,35 @@ from transformers import pipeline
 
 get_completion = pipeline("summarization", model="sshleifer/distilbart-cnn-12-6")
 
-def get_completion(input):
-    try:
-        output = get_completion(input)
-        return output[0]['summary_text']
-    except Exception as e:
-        return str(e)
+#def get_completion(input):
+##    try:
+ #       output = get_completion(input)
+ #       return output[0]['summary_text']
+ #   except Exception as e:
+ #       return str(e)
+
+text = ('''The tower is 324 metres (1,063 ft) tall, about the same height
+        as an 81-storey building, and the tallest structure in Paris. 
+        Its base is square, measuring 125 metres (410 ft) on each side. 
+        During its construction, the Eiffel Tower surpassed the Washington 
+        Monument to become the tallest man-made structure in the world,
+        a title it held for 41 years until the Chrysler Building
+        in New York City was finished in 1930. It was the first structure 
+        to reach a height of 300 metres. Due to the addition of a broadcasting 
+        aerial at the top of the tower in 1957, it is now taller than the 
+        Chrysler Building by 5.2 metres (17 ft). Excluding transmitters, the 
+        Eiffel Tower is the second tallest free-standing structure in France 
+        after the Millau Viaduct.''')
+
+def summarize(input: str) -> str:
+    output = get_completion(input)
+    return output[0]['summary_text']
 
 def create_summarization_tab():
     gr.close_all()
-    tab = gr.Interface(fn=get_completion, 
-                    inputs=[gr.Textbox(label="Text to summarize", lines=6)],
-                    outputs=[gr.Textbox(label="Result", lines=3)],
+    tab = gr.Interface(fn=summarize, 
+                    inputs=[gr.Textbox(label="Text to summarize", lines=6, value=text)],
+                    outputs=[gr.Textbox(label="Result")],
                     title="Text summarization with distilbart-cnn",
                     description="Summarize any text using the `shleifer/distilbart-cnn-12-6` model under the hood!"
                    )
