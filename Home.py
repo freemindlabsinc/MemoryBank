33fa1552bfb7f7246ea7fd4625eeb6717ea9e677
Alessandro Federici <afederici75@users.noreply.github.com>
2024-03-21 12:20:18 -0400

    Fixed home page. I was in the wrong branch

--- Home.py
+++ Home.py
@@ -17,86 +17,15 @@ page_config.initialize_page(
         'About': "# This is a header. This is an *extremely* cool app!"
     }
 )
-#--------------
 
-class FixedAuthenticate(stauth.Authenticate): 
-  def _implement_logout(self):
-        # Clears cookie and session state variables associated with the logged in user.
-        try:
-            self.cookie_manager.delete(self.cookie_name)
-        except Exception as e: 
-            print(e)
-        self.credentials['usernames'][st.session_state['username']]['logged_in'] = False
-        st.session_state['logout'] = True
-        st.session_state['name'] = None
-        st.session_state['username'] = None
-        st.session_state['authentication_status'] = None
+st.write("Welcome to A.I. Of You! ")
+st.write("""
+This is the home page of the application. At some point it will show useful information about the app
+and some diagrams or charts.
 
-with open('config.yaml') as file:
-    config = yaml.load(file, Loader=SafeLoader)
+You can navigate to different modules using the sidebar on the left.
+Only the following modules are functional right now:
+""")
 
-authenticator = FixedAuthenticate(
-    credentials= config['credentials'],
-    cookie_name= config['cookie']['name'],
-    key= config['cookie']['key'],
-    cookie_expiry_days= config['cookie']['expiry_days'],
-    preauthorized= config['preauthorized']
-)
-
-name, authenticated, username = authenticator.login()
-
-if st.session_state["authentication_status"]:
-    authenticator.logout()
-    st.write(f'Welcome *{st.session_state["name"]}*')
-    st.title('Some content')
-    
-elif st.session_state["authentication_status"] is False:
-    st.error('Username/password is incorrect')
-    #st.stop()
-    
-elif st.session_state["authentication_status"] is None:
-    st.warning('Please enter your username and password')
-    
-#--------------
-
-# reset password
-if st.session_state["authentication_status"]:
-    try:
-        if authenticator.reset_password(st.session_state["username"]):
-            st.success('Password modified successfully')
-    except Exception as e:
-        st.error(e)    
-
-# update user details        
-if st.session_state["authentication_status"]:
-    try:
-        if authenticator.update_user_details(st.session_state["username"]):
-            with open('config.yaml', 'w') as file:
-                yaml.dump(config, file, default_flow_style=False)
-                
-            st.success('Entries updated successfully')
-    except Exception as e:
-        st.error(e)        
-        
-# forgot user name
-try:
-    username_of_forgotten_username, email_of_forgotten_username = authenticator.forgot_username()
-    if username_of_forgotten_username:
-        st.success('Username to be sent securely')
-        # The developer should securely transfer the username to the user.
-    elif username_of_forgotten_username == False:
-        st.error('Email not found')
-except Exception as e:
-    st.error(e)        
-    
-# forgot password
-
-try:
-    username_of_forgotten_password, email_of_forgotten_password, new_random_password = authenticator.forgot_password()
-    if username_of_forgotten_password:
-        st.success('New password to be sent securely')
-        # The developer should securely transfer the new password to the user.
-    elif username_of_forgotten_password == False:
-        st.error('Username not found')
-except Exception as e:
-    st.error(e)
+st.page_link("pages/2_📚_Prompt_Library.py", label=":blue[Prompt Library]", icon="📚")
+st.page_link("pages/3_🪄_Prompt_Wizard.py", label=":green[Prompt Wizard]", icon="🪄")
